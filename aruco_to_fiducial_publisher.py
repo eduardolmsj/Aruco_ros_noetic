a68d444b7279dad0f260708f43e6051c8001f38b
eduardo <eduardo.santos@takeandgoapp.com>
2025-07-22 12:19:16 -0300

    changed: aruco_to_fiducial subscribes to /aruco_marker_publisher/markers

--- aruco_to_fiducial_publisher.py
+++ aruco_to_fiducial_publisher.py
@@ -1,58 +1,44 @@
 #!/usr/bin/env python3
 
 import rospy
-import tf
-import tf2_ros
+from aruco_msgs.msg import MarkerArray
 from fiducial_msgs.msg import FiducialTransform, FiducialTransformArray
-from geometry_msgs.msg import TransformStamped
+
+from geometry_msgs.msg import Pose
 
 class ArucoToFiducialPublisher:
     def __init__(self):
         rospy.init_node('aruco_to_fiducial_publisher')
 
-        self.listener = tf.TransformListener()
-        self.publisher = rospy.Publisher('/fiducial_transforms', FiducialTransformArray, queue_size=10)
-        self.rate = rospy.Rate(10)  # 10 Hz
-
         self.camera_frame = rospy.get_param('~camera_frame', 'usb_cam')
-        self.max_ids = rospy.get_param('~max_ids', 50)  # tenta de 0 a 49
+        self.marker_topic = rospy.get_param('~marker_topic', '/aruco_marker_publisher/markers')
 
-        rospy.loginfo("Aruco to Fiducial publisher started")
+        self.publisher = rospy.Publisher('/fiducial_transforms', FiducialTransformArray, queue_size=10)
+        rospy.Subscriber(self.marker_topic, MarkerArray, self.callback)
 
-        while not rospy.is_shutdown():
-            self.publish_transforms()
-            self.rate.sleep()
+        rospy.loginfo(f"Subscribed to {self.marker_topic}")
+        rospy.spin()
 
-    def publish_transforms(self):
+    def callback(self, marker_array_msg):
         msg = FiducialTransformArray()
-        msg.header.stamp = rospy.Time.now()
-        msg.header.frame_id = self.camera_frame
-        msg.image_seq = 0  # você pode incrementar isso se quiser manter sequência
+        msg.header = marker_array_msg.header
+        msg.image_seq = marker_array_msg.header.seq  # You can handle sequence however you like
 
-        for marker_id in range(self.max_ids):
-            frame_id = f"aruco_marker_{marker_id}"
-            try:
-                (trans, rot) = self.listener.lookupTransform(self.camera_frame, frame_id, rospy.Time(0))
-                t = FiducialTransform()
-                t.fiducial_id = marker_id
-                t.transform.translation.x = trans[0]
-                t.transform.translation.y = trans[1]
-                t.transform.translation.z = trans[2]
-                t.transform.rotation.x = rot[0]
-                t.transform.rotation.y = rot[1]
-                t.transform.rotation.z = rot[2]
-                t.transform.rotation.w = rot[3]
+        for marker in marker_array_msg.markers:
+            fid = FiducialTransform()
+            fid.fiducial_id = marker.id
+            fid.transform.translation.x = marker.pose.pose.position.x
+            fid.transform.translation.y = marker.pose.pose.position.y
+            fid.transform.translation.z = marker.pose.pose.position.z
+            fid.transform.rotation = marker.pose.pose.orientation
 
-                msg.transforms.append(t)
+            msg.transforms.append(fid)
 
-            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                continue
-
-        self.publisher.publish(msg)
+        if msg.transforms:
+            self.publisher.publish(msg)
 
 if __name__ == '__main__':
     try:
         ArucoToFiducialPublisher()
     except rospy.ROSInterruptException:
         pass
-
